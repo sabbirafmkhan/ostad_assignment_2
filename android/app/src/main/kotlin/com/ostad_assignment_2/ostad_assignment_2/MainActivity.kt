b2145148e3436b9d678dcea6f9681b536993ad68
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-02-05 10:11:11 +0600

    setup project basic

---
+++ android/app/src/main/kotlin/com/ostad_assignment_2/ostad_assignment_2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ostad_assignment_2.ostad_assignment_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
